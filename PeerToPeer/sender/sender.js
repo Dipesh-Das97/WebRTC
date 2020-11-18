75486af70ab791c48588ceb3362f1b994ef813bf
Dipesh-Das97 <59877761+Dipesh-Das97@users.noreply.github.com>
2020-11-18 20:51:38 +0530

    Application on Peer to Peer Video call

---
+++ PeerToPeer/sender/sender.js
@@ -0,0 +1,79 @@
+const webSocket = new WebSocket("ws://192.168.1.2:3000")
+
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+
+function sendData(data) {
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
+let peerConn
+function startCall() {
+    document.getElementById("video-call-div")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video: true,
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls": ["stun:stun.l.google.com:19302", 
+                    "stun:stun1.l.google.com:19302", 
+                    "stun:stun2.l.google.com:19302"]
+                }
+            ]
+        }
+
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            sendData({
+                type: "store_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        createAndSendOffer()
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+function createAndSendOffer() {
+    peerConn.createOffer((offer) => {
+        sendData({
+            type: "store_offer",
+            offer: offer
+        })
+
+        peerConn.setLocalDescription(offer)
+    }, (error) => {
+        console.log(error)
+    })
+}
---
+++ PeerToPeer/server.js
@@ -0,0 +1,67 @@
+const Socket = require("websocket").server
+const http = require("http")
+const server = http.createServer((req, res) => {})
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000...")
+})
+
+const webSocket = new Socket({ httpServer: server })
+
+let users = []
+
+webSocket.on('request', (req) => {
+    const connection = req.accept()
+
+    connection.on('message', (message) => {
+        const data = JSON.parse(message.utf8Data)
+        users.push({
+            conn : connection
+        });
+        const user = users[0];
+
+        switch(data.type) {
+            case "store_offer":
+                user.offer = data.offer
+                break
+            
+            case "store_candidate":
+                if (user.candidates == null)
+                    user.candidates = []
+                
+                user.candidates.push(data.candidate)
+                break
+            case "send_answer":
+                sendData({
+                    type: "answer",
+                    answer: data.answer
+                }, user.conn)
+                break
+            case "send_candidate":
+                sendData({
+                    type: "candidate",
+                    candidate: data.candidate
+                }, user.conn)
+                break
+            case "join_call":
+
+                sendData({
+                    type: "offer",
+                    offer: user.offer
+                }, connection)
+                
+                user.candidates.forEach(candidate => {
+                    sendData({
+                        type: "candidate",
+                        candidate: candidate
+                    }, connection)
+                })
+
+                break
+        }
+    })
+})
+
+function sendData(data, conn) {
+    conn.send(JSON.stringify(data))
+}
